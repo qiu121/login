268ae657a6bf064239aaf2d05df48de943091f5e
qiu121 <qiu0089@foxmail.com>
2022-12-05 23:11:03 +0800

    🚀 Update

--- sql/login.sql
+++ sql/login.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS login.`account`
 (
     `id`          BIGINT                             NOT NULL AUTO_INCREMENT COMMENT '主键' PRIMARY KEY,
     `user`        VARCHAR(15)                        NOT NULL COMMENT '用户名',
-    `passwd`      VARCHAR(50)                        NOT NULL COMMENT '密码',
+    `password`      VARCHAR(50)                        NOT NULL COMMENT '密码',
     `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间',
     `update_time` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     UNIQUE KEY (`user`)
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS login.`account`
 
 
 -- 添加数据
-INSERT INTO login.`account`(`user`, password)
+INSERT INTO login.`account`(`user`, `password`)
 VALUES ('zhangsan', SHA('123')),
        ('lisi', SHA('234'));
 
